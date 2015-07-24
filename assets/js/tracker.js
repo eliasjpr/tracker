5a2a7967d73c1809278a13c1e3eb1b848bf4bff8
Elias Perez <eliasjpr@gmail.com>
2015-07-24 07:10:44 -0400

    Added more data to track
    Signed-off-by: Elias Perez <eliasjpr@gmail.com>

--- assets/js/tracker.js
+++ assets/js/tracker.js
@@ -1,31 +1,33 @@
 (function (window) {
   "use strict";
 
-  var pinchTrack = window.pinchTrack || {};
+  var pinchMeTrack = window.pinchMeTrack || {};
   var visitId, visitorId, track;
-  var visitTtl  = 4 * 60; // 4 hours
-  var visitorTtl= 2 * 365 * 24 * 60; // 2 years
-  var isReady   = false;
-  var queue     = [];
+  var visitTtl     = 4 * 60; // 4 hours
+  var visitorTtl   = 2 * 365 * 24 * 60; // 2 years
+  var isReady      = false;
+  var queue        = [];
   var canStringify = typeof(JSON) !== "undefined" && typeof(JSON.stringify) !== "undefined";
-  var eventQueue= [];
-  var page      = pinchTrack.page || window.location.pathname;
-  var visitsUrl = pinchTrack.visitsUrl || "/pinchTrack/visits"
-  var eventsUrl = pinchTrack.eventsUrl || "/pinchTrack/events"
+  var eventQueue   = [];
+  var page         = pinchMeTrack.page || window.location.pathname;
+  var visitsUrl    = pinchMeTrack.visitsUrl || "/page"
+  var eventsUrl    = pinchMeTrack.eventsUrl || "/page"
+
+
 
   // cookies
 
   // http://www.quirksmode.org/js/cookies.html
   function setCookie(name, value, ttl) {
-    var expires = "";
+    var expires      = "";
     var cookieDomain = "";
     if (ttl) {
       var date = new Date();
       date.setTime(date.getTime() + (ttl * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
+      expires  = "; expires=" + date.toGMTString();
     }
-    if (pinchTrack.domain) {
-      cookieDomain = "; domain=" + pinchTrack.domain;
+    if (pinchMeTrack.domain) {
+      cookieDomain = "; domain=" + pinchMeTrack.domain;
     }
     document.cookie = name + "=" + escape(value) + expires + cookieDomain + "; path=/";
   }
@@ -33,7 +35,7 @@
   function getCookie(name) {
     var i, c;
     var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
+    var ca     = document.cookie.split(';');
     for (i = 0; i < ca.length; i++) {
       c = ca[i];
       while (c.charAt(0) === ' ') {
@@ -51,7 +53,7 @@
   }
 
   function log(message) {
-    if (getCookie("pinchTrack_debug")) {
+    if (getCookie("pinchMeTrack_debug")) {
       window.console.log(message);
     }
   }
@@ -74,8 +76,8 @@
 
   // http://stackoverflow.com/a/2117523/1177228
   function generateId() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
@@ -83,31 +85,34 @@
   function saveEventQueue() {
     // TODO add stringify method for IE 7 and under
     if (canStringify) {
-      setCookie("pinchTrack_events", JSON.stringify(eventQueue), 1);
+      setCookie("pinchMeTrack_events", JSON.stringify(eventQueue), 1);
     }
   }
 
   function trackEvent(event) {
-    ready( function () {
+    ready(function () {
       // ensure JSON is defined
       if (canStringify) {
-        var event = JSON.stringify(event)
 
-        if ("WebSocket" in window)
-        {
-          io.socket.post('/page', event, function (data, jwres){
-            deQueue(event)
+        if ("WebSocket" in window) {
+
+          io.socket.post(eventsUrl, event, function (data, jwres) {
+            deQueue(data)
+            log("=== Event Saved! ===")
+            log(data)
           });
         }
-        else{
+        else {
           $.ajax({
-            type: "POST",
-            url: eventsUrl,
-            data: event ,
+            type       : "POST",
+            url        : eventsUrl,
+            data       : event,
             contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function() {
+            dataType   : "json",
+            success    : function () {
               deQueue(event)
+              log("=== Event Saved! ===")
+              log(event)
             }
           });
         }
@@ -116,7 +121,7 @@
     });
   }
 
-  function deQueue(event){
+  function deQueue(event) {
     // remove from queue
     for (var i = 0; i < eventQueue.length; i++) {
       if (eventQueue[i].id == event.id) {
@@ -127,23 +132,55 @@
     saveEventQueue();
   }
 
-
   function eventProperties(e) {
     var $target = $(e.currentTarget);
+
     return {
-      tag: $target.get(0).tagName.toLowerCase(),
-      id: $target.attr("id"),
+      tag    : $target.get(0).tagName.toLowerCase(),
+      id     : $target.attr("id"),
       "class": $target.attr("class"),
-      page: page,
+      page   : page,
       section: $target.closest("*[data-section]").data("section")
     };
   }
 
-  // main
+  function appendData(){
+    return {
+      userId       : 0,
+      campaign     : 0,
+      title        : window.document.title,
+      url          : window.document.URL,
+      referrer     : window.document.referrer,
+      domain       : window.document.domain,
+      docType      : window.document.doctype,
+      cookie       : window.document.cookie,
+      screen       : {
+        availHeight  : screen.availHeight,
+        availWidth   : screen.availWidth,
+        colorDepth   : screen.colorDepth,
+        screen_height: screen.height,
+        pixelDepth   : screen.pixelDepth,
+        screen_width : screen.width
+      },
+      navigator    : {
+        appCodeName  : window.navigator.appCodeName,
+        appName      : window.navigator.appName,
+        appVersion   : window.navigator.appVersion,
+        cookieEnabled: window.navigator.cookieEnabled,
+        language     : window.navigator.language,
+        userLanguage : window.navigator.userLanguage,
+        onLine       : window.navigator.onLine,
+        platform     : window.navigator.platform,
+        product      : window.navigator.product,
+        userAgent    : window.navigator.userAgent
+      }
+    };
+  }
 
-  visitId = getCookie("pinchTrack_visit");
-  visitorId = getCookie("pinchTrack_visitor");
-  track = getCookie("pinchTrack_track");
+  // main
+  visitId   = getCookie("pinchMeTrack_visit");
+  visitorId = getCookie("pinchMeTrack_visitor");
+  track     = getCookie("pinchMeTrack_track");
 
   if (visitId && visitorId && !track) {
     // TODO keep visit alive?
@@ -151,156 +188,140 @@
     setReady();
   }
   else {
-    if(track) {
-      destroyCookie("pinchTrack_track");
+    if (track) {
+      destroyCookie("pinchMeTrack_track");
     }
 
     if (!visitId) {
       visitId = generateId();
-      setCookie("pinchTrack_visit", visitId, visitTtl);
+      setCookie("pinchMeTrack_visit", visitId, visitTtl);
     }
 
     // make sure cookies are enabled
-    if (getCookie("pinchTrack_visit")) {
+    if (getCookie("pinchMeTrack_visit")) {
       log("Visit started");
 
       if (!visitorId) {
         visitorId = generateId();
-        setCookie("pinchTrack_visitor", visitorId, visitorTtl);
+        setCookie("pinchMeTrack_visitor", visitorId, visitorTtl);
       }
 
-      var data = {
-        visit_token:    visitId,
-        visitor_token:  visitorId,
-        landing_page:   window.location.href,
-        userId:         0,
-        campaign:       0,
-        title:          window.document.title,
-        url:            window.document.URL,
-        referrer:       window.document.referrer,
-        domain:         window.document.domain,
-        docType:        window.document.doctype,
-        cookie:         window.document.cookie,
-        screen: {
-          availHeight:    screen.availHeight,
-          availWidth:     screen.availWidth,
-          colorDepth:     screen.colorDepth,
-          screen_height:  screen.height,
-          pixelDepth:     screen.pixelDepth,
-          screen_width:   screen.width
-        },
-        navigator:{
-          appCodeName: window.navigator.appCodeName,
-          appName: window.navigator.appName,
-          appVersion: window.navigator.appVersion,
-          cookieEnabled: window.navigator.cookieEnabled,
-          language: window.navigator.language,
-          userLanguage: window.navigator.userLanguage,
-          onLine: window.navigator.onLine,
-          platform: window.navigator.platform,
-          product: window.navigator.product,
-          userAgent: window.navigator.userAgent
-        }
-
+      var event = {
+        visit_token  : visitId,
+        visitor_token: visitorId,
+        landing_page : window.location.href
       };
 
 
-      $.post(visitsUrl, data, setReady, "json");
 
-    } else {
+      if("WebSocket" in window){
+        io.socket.post(visitsUrl, event, function (data, jwres) {
+          log("=== Visit Saved! ===")
+          log(data)
+        });
+      }else{
+        $.post(visitsUrl, event, setReady, "json");
+      }
+    }
+    else {
       log("Cookies disabled");
       setReady();
     }
   }
 
-  pinchTrack.getVisitId = pinchTrack.getVisitToken = function () {
+  pinchMeTrack.getVisitId = pinchMeTrack.getVisitToken = function () {
     return visitId;
   };
 
-  pinchTrack.getVisitorId = pinchTrack.getVisitorToken = function () {
+  pinchMeTrack.getVisitorId = pinchMeTrack.getVisitorToken = function () {
     return visitorId;
   };
 
-  pinchTrack.reset = function () {
-    destroyCookie("pinchTrack_visit");
-    destroyCookie("pinchTrack_visitor");
-    destroyCookie("pinchTrack_events");
-    destroyCookie("pinchTrack_track");
+  pinchMeTrack.reset = function () {
+    destroyCookie("pinchMeTrack_visit");
+    destroyCookie("pinchMeTrack_visitor");
+    destroyCookie("pinchMeTrack_events");
+    destroyCookie("pinchMeTrack_track");
     return true;
   };
 
-  pinchTrack.debug = function (enabled) {
+  pinchMeTrack.debug = function (enabled) {
     if (enabled === false) {
-      destroyCookie("pinchTrack_debug");
+      destroyCookie("pinchMeTrack_debug");
     } else {
-      setCookie("pinchTrack_debug", "t", 365 * 24 * 60); // 1 year
+      setCookie("pinchMeTrack_debug", "t", 365 * 24 * 60); // 1 year
     }
     return true;
   };
 
-  pinchTrack.track = function (name, properties) {
+  pinchMeTrack.track = function (name, properties) {
     // generate unique id
     var event = {
-      id: generateId(),
-      name: name,
-      properties: properties,
-      time: (new Date()).getTime() / 1000.0
+      id           : generateId(),
+      name         : name,
+      event        : properties,
+      time         : (new Date()).getTime() / 1000.0,
+      visit_token  : visitId,
+      visitor_token: visitorId,
+      landing_page : window.location.href,
+      data: appendData()
     };
-    log(event);
 
     eventQueue.push(event);
     saveEventQueue();
 
     // wait in case navigating to reduce duplicate events
-    setTimeout( function () {
+    setTimeout(function () {
       trackEvent(event);
     }, 1000);
   };
 
-  pinchTrack.trackView = function () {
+  pinchMeTrack.trackView = function () {
     var properties = {
-      url: window.location.href,
+      url  : window.location.href,
       title: document.title,
-      page: page
+      page : page
     };
-    pinchTrack.track("$view", properties);
+    pinchMeTrack.track("$view", properties);
   };
 
-  pinchTrack.trackClicks = function () {
+  pinchMeTrack.trackClicks = function () {
     $(document).on("click", "a, button, input[type=submit]", function (e) {
-      var $target = $(e.currentTarget);
-      var properties = eventProperties(e);
+      var $target     = $(e.currentTarget);
+      var properties  = eventProperties(e);
+
       properties.text = properties.tag == "input" ? $target.val() : $.trim($target.text().replace(/[\s\r\n]+/g, " "));
       properties.href = $target.attr("href");
-      pinchTrack.track("$click", properties);
+      pinchMeTrack.track("$click", properties);
     });
   };
 
-  pinchTrack.trackSubmits = function () {
+  pinchMeTrack.trackSubmits = function () {
     $(document).on("submit", "form", function (e) {
       var properties = eventProperties(e);
-      pinchTrack.track("$submit", properties);
+      pinchMeTrack.track("$submit", properties);
     });
   };
 
-  pinchTrack.trackChanges = function () {
+  pinchMeTrack.trackChanges = function () {
     $(document).on("change", "input, textarea, select", function (e) {
       var properties = eventProperties(e);
-      pinchTrack.track("$change", properties);
+      pinchMeTrack.track("$change", properties);
     });
   };
 
-  pinchTrack.trackAll = function() {
-    pinchTrack.trackView();
-    pinchTrack.trackClicks();
-    pinchTrack.trackSubmits();
-    pinchTrack.trackChanges();
+  pinchMeTrack.trackAll = function () {
+    pinchMeTrack.trackView();
+    pinchMeTrack.trackClicks();
+    pinchMeTrack.trackSubmits();
+    pinchMeTrack.trackChanges();
   };
 
   // push events from queue
   try {
-    eventQueue = JSON.parse(getCookie("pinchTrack_events") || "[]");
-  } catch (e) {
+    eventQueue = JSON.parse(getCookie("pinchMeTrack_events") || "[]");
+  }
+  catch (e) {
     // do nothing
   }
 
@@ -308,5 +329,5 @@
     trackEvent(eventQueue[i]);
   }
 
-  window.pinchTrack = pinchTrack;
+  window.pinchMeTrack = pinchMeTrack;
 }(window));
