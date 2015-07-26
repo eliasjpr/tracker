a592c309071bf8ec66767858f4ccb8fa393082a8
Elias Perez <eliasjpr@gmail.com>
2015-07-26 12:17:44 -0400

    Added Pages Store model
    Signed-off-by: Elias Perez <eliasjpr@gmail.com>

---
+++ assets/lib/models/PagesStore.js
@@ -0,0 +1,22 @@
+function PagesStore(){
+  self = this
+  riot.observable(this),
+  self.pages = [];
+
+  self.load = function(data){
+    self.pages = data
+    self.trigger('pages-loaded', self.pages)
+  }
+
+  self.add = function(item){
+    self.pages.push(item)
+    self.trigger('page-added', self.pages)
+  }
+
+  self.count = function(type){
+    self.count  =  $.grep(self.pages, function(v) {
+        return v.name === type;
+    });
+  }
+
+}
