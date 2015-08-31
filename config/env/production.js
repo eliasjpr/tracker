8d316b31c3ac41ee1716676cf67ffe1f0f33ef04
Elias Perez <eliasjpr@gmail.com>
2015-08-31 11:37:30 -0400

    set correct username and password, port and environment
    Signed-off-by: Elias Perez <eliasjpr@gmail.com>

--- config/env/production.js
+++ config/env/production.js
@@ -4,14 +4,14 @@ module.exports = {
       adapter: 'sails-mongo',
       host: 'ec2-23-21-222-200.compute-1.amazonaws.com',
       port: 27017,
-      // user: 'liana_user',
-      // password: 'PINCHme123',
+      user: 'liana_user',
+      password: 'PINCHme123',
       database: 'lianadb_dev'
     }
   },
   models: {
      connection: 'mongoLive'
   },
-  port: '80',
-  environment: 'production'
+  port: process.env.PORT || '80',
+  environment: process.env.NODE_ENV || 'production'
 };
