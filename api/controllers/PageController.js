b886a95276b8375eaec81310a72fea60f1a84ad4
Elias Perez <eliasjpr@gmail.com>
2015-08-31 11:36:59 -0400

    Remove console log
    Signed-off-by: Elias Perez <eliasjpr@gmail.com>

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -13,22 +13,24 @@ module.exports = {
     request('http://www.telize.com/geoip/108.176.21.134')
       .on('error', function(error){ console.log(error) } )
       .on('response', function(response){
+
         req.body.geolocation = response.body;
-        console.log(req.body.geolocation)
+        req.body.ip       = req.ip || req.socket.handshake.address || '0.0.0.0';
+        req.body.headers  = req.headers || req.socket.handshake.headers;
+        req.body.secure   = req.secure;
+        req.body.query    = req.query;
+        req.body.params   = req.params;
+        req.body.user     = req.cookies.uxid || 0;
+        req.body.campaign = req.cookies.cxid || 0;
+
+        Page.create(req.body).exec(function createCB(err, page){
+          if(err){ return res.json( 500, err); }
+          req.isSocket ? Page.publishCreate(page) : res.json(200, page);
+        });
+
       });
 
-    req.body.ip       = req.ip || req.socket.handshake.address || '0.0.0.0';
-    req.body.headers  = req.headers || req.socket.handshake.headers;
-    req.body.secure   = req.secure;
-    req.body.query    = req.query;
-    req.body.params   = req.params;
-    req.body.user     = req.cookies.uxid || 0;
-    req.body.campaign = req.cookies.cxid || 0;
 
-    Page.create(req.body).exec(function createCB(err, page){
-      if(err){ return res.json( 500, err); }
-      req.isSocket ? Page.publishCreate(page) : res.json(200, page);
-    });
   }
 };
 
